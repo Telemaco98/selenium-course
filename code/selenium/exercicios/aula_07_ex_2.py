81378a6386dcb97014350b2e8a7a5c2ad070ddf6
Telemaco98 <shirleytelemaco98@gmail.com>
2025-01-21 23:19:42 -0300

    :white_check_mark: Resolving class 7 - exercise 2

--- code/selenium/exercicios/aula_07_ex_2.py
+++ code/selenium/exercicios/aula_07_ex_2.py
@@ -7,14 +7,58 @@
 
 from selenium.webdriver import Firefox
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import StaleElementReferenceException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.support.events import (
+    AbstractEventListener,
+    EventFiringWebDriver
+)
+from urllib.parse import urlparse
 import time
+
+class Listener(AbstractEventListener):
+    def after_navigate_to(self, url, webdriver):
+        print(f'the url had changed: {url}')
+
+    def before_click(self, element, webdriver):
+        print(f'cliquei no "{element.text}"')
+
+def answer_click(browser, answer_should_be):
+    _as = browser_wrapper.find_elements(By.CSS_SELECTOR, 'main a')
+    path = urlparse(browser.current_url).path
+
+    for a in _as:
+        if a.get_attribute("attr") == answer_should_be:
+            return a
+        elif a.get_attribute("attr") == "" and a.text == path[1:]:
+            return a
 
 url = "https://curso-python-selenium.netlify.app/exercicio_03.html"
 
 browser = Firefox()
-browser.implicitly_wait(1)
-browser.get(url)
+browser_wrapper = EventFiringWebDriver(browser, Listener())
+browser_wrapper.implicitly_wait(2)
+browser_wrapper.get(url)
 
-# TODO
+start = browser_wrapper.find_element(By.CSS_SELECTOR, 'main a')
+start.click()
+
+win = False
+
+while not win:
+    try:
+        command = browser_wrapper.find_element(By.CSS_SELECTOR, 'main > p')
+        if "certo" in command.text:
+            answer_click(browser_wrapper, "certo").click()
+        elif "contrário" in command.text:
+            answer_click(browser_wrapper, "errado").click()
+        elif "refresh" in command.text:
+            browser_wrapper.refresh()
+    except NoSuchElementException as e:
+        print("the end")
+        win = True
+    except StaleElementReferenceException as e:
+        print("Catched StaleElementReferenceException")
+
 
 browser.quit()
