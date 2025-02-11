8c9a99c683d6b8fdf8568927adb778869fcd891e
Telemaco98 <shirleytelemaco98@gmail.com>
2025-02-10 23:35:46 -0300

    :memo: Aula 08 - Part II - Mouse events

---
+++ code/selenium/exercicios/aula_08_ex_1.py
@@ -0,0 +1,8 @@
+"""
+    Dizer todas as cores possíveis da caixinha
+    https://curso-python-selenium.netlify.app/caixinha
+    # Spoiler: são 17 cores
+"""
+
+
+url = "https://curso-python-selenium.netlify.app/caixinha"
